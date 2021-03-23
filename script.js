9e9e12e5cdb9909342d388ba4e51ebac451b002d
abhi7839 <73695543+abhi7839@users.noreply.github.com>
2021-03-23 12:01:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.img-btn').addEventListener('click', function () {
+    "use strict";
+    document.querySelector('.cont').classList.toggle('s-signup');
+}
+                                                   );
